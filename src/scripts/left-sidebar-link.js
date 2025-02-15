2532581455a2476b7fff972e1701431cf9ef5816
golam71 <74001687+golam71@users.noreply.github.com>
2025-02-15 23:43:03 +0600

    :bug: Adjust IntersectionObserver rootMargin for better active link highlighting

--- src/scripts/left-sidebar-link.js
+++ src/scripts/left-sidebar-link.js
@@ -1,30 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const blog = document.querySelector("main.animate");
-    const tocItems = document.querySelectorAll(".toc-item");
-    const headings = blog.querySelectorAll("h1, h2, h3, h4, h5, h6");
+	const blog = document.querySelector("main.animate");
+	const tocItems = document.querySelectorAll(".toc-item");
+	const headings = blog.querySelectorAll("h1, h2, h3, h4, h5, h6");
 
-    const observer = new IntersectionObserver(
-        (entries) => {
-            for (const entry of entries) {
-                if (entry.isIntersecting) {
-                    const targetId = entry.target.id;
-                    for (const item of tocItems) {
-                        item.classList.remove("active-table-of-contents-link");
-                        if (item.getAttribute("href") === `#${targetId}`) {
-                            item.classList.add("active-table-of-contents-link");
-                        }
-                    }
-                }
-            }
-        },
-        {
-            root: null,
-            rootMargin: "-100px 0px 0px 0px",
-            threshold: 1, 
-        },
-    );
+	const observer = new IntersectionObserver(
+		(entries) => {
+			for (const entry of entries) {
+				if (entry.isIntersecting) {
+					const targetId = entry.target.id;
+					for (const item of tocItems) {
+						item.classList.remove("active-table-of-contents-link");
+						if (item.getAttribute("href") === `#${targetId}`) {
+							item.classList.add("active-table-of-contents-link");
+						}
+					}
+				}
+			}
+		},
+		{
+			root: null,
+			rootMargin: "0px 0px -70% 0px",
+			threshold: 1, // Adjust this value as needed
+		},
+	);
 
-    for (const heading of headings) {
-        observer.observe(heading);
-    }
+	for (const heading of headings) {
+		observer.observe(heading);
+	}
 });
